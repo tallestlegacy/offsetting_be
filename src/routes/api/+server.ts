894f7e8394fb2c8ba429e995f0d1380b131e561f
marvin <tallestlegacy@gmail.com>
2023-02-08 09:42:32 +0300

    added projects

---
+++ src/routes/api/+server.ts
@@ -0,0 +1,24 @@
+import { json } from "@sveltejs/kit";
+
+export async function GET() {
+	const query = `
+    *[_type == 'project']{
+        'heroImageUrl' : hero.asset -> url,
+        title,
+        locationName,
+        lon,
+        lat,
+        createdDate,
+        description,
+        story,
+        requiredFunds,
+        allocatedFunds
+      }
+    `;
+	const res = await fetch(`https://hqypioh7.api.sanity.io/v2021-10-21/data/query/projects?query=${query}`);
+	const data = await res.json();
+
+	const projects = data.result;
+
+	return json(projects);
+}
